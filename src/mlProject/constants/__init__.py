5bb6934a7e109af620ca7908a8bbf8ca54cb1626
kaushik1064 <srinukaus@gmail.com>
2023-11-20 14:13:06 +0530

    data ingestion added

---
+++ src/mlProject/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+CONFIG_FILE_PATH = Path("D:\\End-End-ML-project\\config\\config.yaml")
+PARAMS_FILE_PATH = Path("D:\\End-End-ML-project\\params.yaml")
+SCHEMA_FILE_PATH = Path("D:\\End-End-ML-project\\schema.yaml")
